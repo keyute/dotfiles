c210e855b93ce2748abcb04180f9d2a9b097ec4c
Byran Low <git@lowbryan.com>
2023-08-25 16:47:21 +1000

    more changes

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,24 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-refactor"
+  },
   opts = {
     ensure_installed = {
       "lua", "json", "yaml", "toml", "bash", "terraform", "python", "markdown", "markdown_inline", "mermaid", "java",
       "kotlin"
     },
     highlight = { enable = true },
-    indent = { enable = true }
+    indent = { enable = true },
+    refactor = {
+      highlight_definitions = { enable = true },
+      highlight_current_scope = { enable = true },
+      smart_rename = {
+        enable = true,
+        keymaps = { smart_rename = "grr" }
+      }
+    }
   },
   main = "nvim-treesitter.configs"
 }
