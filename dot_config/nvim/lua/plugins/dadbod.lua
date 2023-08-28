f2b8caca6a39e323a227579881e9465b50006cc5
Byran Low <git@lowbryan.com>
2023-08-29 01:58:56 +1000

    database

---
+++ dot_config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,10 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    'tpope/vim-dadbod'
+  },
+  cmd = "DBUI",
+  keys = {
+    {"<leader>db", "<cmd>DBUI<cr>", "Open Dadbod UI"}
+  }
+}
