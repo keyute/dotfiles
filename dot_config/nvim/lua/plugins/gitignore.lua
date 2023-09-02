e5127d5da04302c476a81469444fbb04c00bc9e9
Byran Low <git@lowbryan.com>
2023-09-02 23:35:41 +1000

    gitignore

--- dot_config/nvim/lua/plugins/gitignore.lua
+++ dot_config/nvim/lua/plugins/gitignore.lua
@@ -1,5 +1,8 @@
 return {
   "wintermute-cell/gitignore.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim"
+  },
   cmd = "Gitignore",
   keys = {
     {"<leader>gi", "<cmd>Gitignore<cr>", desc = "Generate gitignore"}
