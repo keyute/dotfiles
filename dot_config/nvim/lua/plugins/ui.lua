695c40cd26585e66d33209c391b085e3bb98f1f4
Byran Low <git@lowbryan.com>
2023-09-11 19:39:13 +1000

    update

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -35,15 +35,10 @@ return {
           delay = 0,
           reveal = { 'close' }
         },
-        diagnostics = "coc",
-        diagnostics_indicator = function(_, _, diagnostics_dict, _)
-          local s = " "
-          for e, n in pairs(diagnostics_dict) do
-            local sym = e == "error" and " "
-                or (e == "warning" and " " or "")
-            s = s .. n .. sym
-          end
-          return s
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, _, _)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
         end
       }
     }
