03ffe6ab57eab866f67a9c974ed1b0390607fb9e
Byran Low <git@lowbryan.com>
2023-10-13 12:09:58 +1100

    4 spaces instead of 2

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -7,9 +7,9 @@ vim.o.clipboard = 'unnamed'
 vim.o.colorcolumn = "120"
 vim.o.cursorline = true
 vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 
 local keyset = vim.keymap.set
 local keyopts = { silent = true, noremap = true }
