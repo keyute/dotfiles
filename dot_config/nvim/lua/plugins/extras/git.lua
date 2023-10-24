f012bfbc88c39eb87ea05f90a534eddd3fa63606
Byran Low <git@lowbryan.com>
2023-10-25 00:12:01 +1100

    add neogit

---
+++ dot_config/nvim/lua/plugins/extras/git.lua
@@ -0,0 +1,13 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "sindrets/diffview.nvim",
+        "ibhagwan/fzf-lua"
+    },
+    opts = {},
+    keys = {
+        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" }
+    }
+}
