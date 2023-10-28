b3fe17c8bfcd5819b108bce39732499ce7cf6522
Byran Low <git@lowbryan.com>
2023-10-28 17:20:59 +1100

    markdown theme

--- dot_config/nvim/lua/plugins/extras/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -2,7 +2,8 @@ return {
   "saimo/peek.nvim",
   build = "deno task --quiet build",
   opts = {
-    app = 'browser'
+    app = 'browser',
+    theme = vim.g.material_style == 'lighter' and 'light' or 'dark'
   },
   keys = {
     {
