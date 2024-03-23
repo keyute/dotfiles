b13faa088cab4db8543d587e43cd5f559691060b
Byran Low <git@lowbryan.com>
2024-03-23 15:33:48 +1100

    add copilot chat

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -198,5 +198,21 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify"
     }
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+      "nvim-lua/plenary.nvim"
+    },
+    opts = {},
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<cr>",   desc = "Toggle Copilot Chat" },
+      { "<leader>ce", "<cmd>CopilotChatExplain<cr>",  desc = "Copilot Explain Code" },
+      { "<leader>ct", "<cmd>CopilotChatTests<cr>",    desc = "Copilot Generate Tests" },
+      { "<leader>cf", "<cmd>CopilotChatFix<cr>",      desc = "Copilot Fix Code" },
+      { "<leader>co", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot Optimize Code" },
+      { "<leader>cd", "<cmd>CopilotChatDocs<cr>",     desc = "Copilot Generate Docs" }
+    }
   }
 }
