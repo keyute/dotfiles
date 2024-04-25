475524acd85d4462e3359c26e01036181aa97d4c
Bryan Low <git@lowbryan.com>
2024-04-26 02:54:49 +1000

    octo

--- dot_config/nvim/lua/plugins/extras/git.lua
+++ dot_config/nvim/lua/plugins/extras/git.lua
@@ -33,4 +33,14 @@ return {
 			},
 		},
 	},
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+		cmd = "Octo",
+	},
 }
