dfcdc2fe847f5ba2c054e62b6dcbc564c66f765e
Bryan Low <git@lowbryan.com>
2024-05-15 01:08:54 +1000

    add markdown

--- dot_config/nvim/lua/plugins/extras/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -1,19 +1,36 @@
 return {
-	"toppair/peek.nvim",
-	build = "deno task --quiet build:fast",
-	config = function()
-		require("peek").setup({
-			app = "browser",
-			theme = vim.g.material_style == "lighter" and "light" or "dark",
-		})
-	end,
-	keys = {
-		{
-			"<leader>p",
-			function()
-				require("peek").open()
-			end,
-			desc = "Preview Markdown",
+	{
+		"toppair/peek.nvim",
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup({
+				app = "browser",
+				theme = vim.g.material_style == "lighter" and "light" or "dark",
+			})
+		end,
+		keys = {
+			{
+				"<leader>p",
+				function()
+					require("peek").open()
+				end,
+				desc = "Preview Markdown",
+			},
+		},
+	},
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {},
+		ft = "markdown",
+		keys = {
+			{
+				"<leader>m",
+				function()
+					require("render-markdown").toggle()
+				end,
+				desc = "Toggle Markdown Render",
+			},
 		},
 	},
 }
