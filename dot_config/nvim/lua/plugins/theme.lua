214cb54ca153b85c89166f60571700ee2f26fd63
Bryan Low <its@blow.sg>
2024-06-12 17:29:41 +1000

    auto dark mode

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -8,6 +8,21 @@ return {
 		end,
 	},
 	{
+		"f-person/auto-dark-mode.nvim",
+		event = "VeryLazy",
+		opts = {
+			update_interval = 1000,
+			set_dark_mode = function()
+				vim.o.background = "dark"
+				vim.cmd("colorscheme catppuccin")
+			end,
+			set_light_mode = function()
+				vim.o.background = "light"
+				vim.cmd("colorscheme catppuccin")
+			end,
+		}
+	},
+	{
 		"glepnir/dashboard-nvim",
 		event = "VimEnter",
 		opts = {
