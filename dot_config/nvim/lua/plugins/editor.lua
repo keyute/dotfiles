ee18279efb33307936c3bfe11ae16c29c1aaf67d
Bryan Low <github@a.blow.sg>
2025-08-15 11:47:50 +1000

    conform

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -207,7 +207,10 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "black" },
-				javascript = { "prettierd" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 				go = { "gofmt", "goimports" },
 				markdown = { "prettierd" },
 				yaml = { "yamlfmt" },
