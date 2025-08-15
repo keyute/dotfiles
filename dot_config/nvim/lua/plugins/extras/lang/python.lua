65a15753106649ab1fe82d0a9e092e704038bfdb
Bryan Low <github@a.blow.sg>
2025-08-16 02:29:06 +1000

    use kickstart

--- dot_config/nvim/lua/plugins/extras/lang/python.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"linux-cultist/venv-selector.nvim",
-	branch = "regexp",
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
-	opts = {},
-	keys = {
-		{ "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select Python Venv" },
-	},
-}
