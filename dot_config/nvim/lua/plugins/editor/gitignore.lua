556a34dcca796b961629434c6e6fc3de02542828
Bryan Low <github@a.blow.sg>
2025-08-28 18:04:36 +1000

    gitignore

---
+++ dot_config/nvim/lua/plugins/editor/gitignore.lua
@@ -0,0 +1,10 @@
+return {
+	"wintermute-cell/gitignore.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
+	opts = {},
+	keys = {
+		{ "<leader>gi", "<cmd>Gitignore<cr>", desc = { "[G]it[I]gnore" } },
+	},
+}
