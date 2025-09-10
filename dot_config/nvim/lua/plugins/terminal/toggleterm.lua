02c4a2fa6a1ca8930de601d87fbc8fcfc8228dde
Bryan Low <github@a.blow.sg>
2025-09-11 01:11:18 +1000

    toggle term

--- dot_config/nvim/lua/plugins/terminal/toggleterm.lua
+++ dot_config/nvim/lua/plugins/terminal/toggleterm.lua
@@ -2,8 +2,8 @@ return {
 	"akinsho/toggleterm.nvim",
 	opts = {},
 	keys = {
-		{ "<leader>tt", "<cmd>ToggleTerm direction=tab<cr>", desc = "Terminal in New Tab" },
-		{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal in Vertical Tab" },
-		{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Terminal in Horizontal Tab" },
+		{ "<leader>tt", "<cmd>ToggleTerm direction=tab<cr>", desc = "[T]erminal in New [T]ab" },
+		{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "[T]erminal in [V]ertical Tab" },
+		{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "[T]erminal in [H]orizontal Tab" },
 	},
 }
