caaa7a2939323c707fdb4883ba707daa938609ba
Bryan Low <keyute@Bryans-MacBook-Pro.local>
2025-10-09 05:58:05 +1100

    bugfixes

--- dot_config/nvim/lua/plugins/editor/opencode.lua
+++ dot_config/nvim/lua/plugins/editor/opencode.lua
@@ -3,5 +3,51 @@ return {
 	dependencies = {
 		{ "folke/snacks.nvim", opts = { input = {}, picker = {} } },
 	},
-	keys = {},
+	keys = {
+		{
+			"<leader>oa",
+			function()
+				require("opencode").ask("", { submit = true })
+			end,
+			desc = "[O]pencode [A]sk",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>o+",
+			function()
+				require("opencode").prompt("@this")
+			end,
+			desc = "[O]pencode Add [+]",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>os",
+			function()
+				require("opencode").select()
+			end,
+			desc = "[O]pencode [S]elect",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>ot",
+			function()
+				require("opencode").toggle()
+			end,
+			desc = "[O]pencode [T]oggle",
+		},
+		{
+			"<leader>on",
+			function()
+				require("opencode").command("session_new")
+			end,
+			desc = "[O]pencode [N]ew",
+		},
+		{
+			"<leader>oi",
+			function()
+				require("opencode").command("session_interrupt")
+			end,
+			desc = "[O]pencode [I]nterrupt",
+		},
+	},
 }
