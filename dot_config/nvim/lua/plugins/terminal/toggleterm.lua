d57b644eb7c155ce9af05c9dc5f83a768ae39538
Bryan Low <github@a.blow.sg>
2026-05-14 16:09:03 +1000

    remove unnecessary plugins

--- dot_config/nvim/lua/plugins/terminal/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"akinsho/toggleterm.nvim",
-	opts = {},
-	keys = {
-		{ "<leader>tt", "<cmd>ToggleTerm direction=tab<cr>", desc = "[T]erminal in New [T]ab" },
-		{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "[T]erminal in [V]ertical Tab" },
-		{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "[T]erminal in [H]orizontal Tab" },
-	},
-}
