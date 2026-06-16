f400a2a8cb6cc7e19422a1c381a24d48160a0bd7
Bryan Low <github@a.blow.sg>
2026-06-16 11:20:21 +1000

    fix telescope

--- dot_config/nvim/lua/plugins/editor/telescope.lua
+++ dot_config/nvim/lua/plugins/editor/telescope.lua
@@ -11,7 +11,26 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob=!**/.git/*",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		})
 
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
