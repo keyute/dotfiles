66b4947d603a815378e7d866495399ccefb95a94
Bryan Low <github@a.blow.sg>
2026-08-03 10:57:18 +1000

    remove unneeded stuff

--- dot_config/nvim/lua/plugins/editor/git.lua
+++ dot_config/nvim/lua/plugins/editor/git.lua
@@ -5,16 +5,16 @@ return {
 		opts = {},
 	},
 	{
-		"akinsho/git-conflict.nvim",
-		version = "*",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {},
-	},
-	{
 		-- actively maintained drop-in fork of sindrets/diffview.nvim, whose
 		-- upstream has been idle since 2024
 		"dlyongemallo/diffview-plus.nvim",
 		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+		opts = {
+			view = {
+				-- entered from lazygit purely to resolve, so skip the file panel
+				merge_tool = { focus_diff = true },
+			},
+		},
 		keys = {
 			{ "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "[D]iffview [O]pen" },
 			{ "<leader>df", "<cmd>DiffviewFileHistory<cr>", desc = "[D]iffview [F]ile History" },
